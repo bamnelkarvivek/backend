0996c5424aede1f8a632808dad159fb950a979f0
bamnelkarvivek <bamnelkarvivek@gmail.com>
2024-10-04 13:22:39 -0400

    Fixed CORS error

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.auth_routes import auth_blueprint
 from routes.file_routes import file_blueprint
 
 app = Flask(__name__)
+
+CORS(app) # This will enable CORS for all routes
 
 # Register blueprints
 app.register_blueprint(auth_blueprint)
